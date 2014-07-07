9b95baa43573cb76398667637326aa075c144a35
Desi <facebookaa@abv.bg>
2014-07-07 20:36:14 +0300

    Version 3
    Added scoring in gameplay.

--- rockpaperscissors_hash.rb
+++ rockpaperscissors_hash.rb
@@ -1,14 +1,22 @@
-3.times do
+score = [0, 0]
+loop do
   rules = {'rock' => 'scissors', 'scissors' => 'paper', 'paper' => 'rock'}
   moves = rules.keys
+  
 
   puts <<-EOF
----------------------------------
-Choose one: Rock, Paper, Scissors
-----------------------------------
+-------------------------------------------------------------
+Choose one: Rock, Paper, Scissors or Exit (to stop teh game)
+-------------------------------------------------------------
 EOF
   puts "You choose: "
   user_move = gets.chomp
+  
+  if user_move == "exit"
+    puts "Good Bye!"
+    break
+  end
+
   if not moves.include? user_move
     puts "Wrong move: #{user_move}"
     next
@@ -23,7 +31,11 @@ EOF
     puts "It's a tie."
   elsif rules[user_move] == comp_move
     puts "You win!"
+    score[0] += 1
   else
     puts "Sorry, you lose!"
+    score[1] += 1
   end
+  puts "Player score: = #{score[0]}, Computer score: = #{score[1]}"
 end
+
